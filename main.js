102067b7dddcebabfb156f9ed2e877cf266cb342
tang802 <tang.meng@husky.neu.edu>
2022-02-15 20:05:26 -0500

    task 2

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
-// function clickButton () {
-//     let number = 0;
-//     let number_click = parseInt(number.innerHTML);
-//     number_click++;
-//     document.getElementsByClassName("item").innerHTML = number_click;
-//     console.log(number_click);
-//  }
+function countNumber(){
+    let circleSelector = document.getElementById("counter").textContent;
+    let current_num = Number(circleSelector) + 1;
+    document.getElementById("counter").textContent = String(current_num);
+    // constant variable -> by id -> reference to the circle -> add lisenter and handler -> use for counter function
 
-var circleSelector = document.getElementsByClassName("item");
-var function_click = function() {
-    var last_number = +this.innerHTML || 0; 
-    this.innerHTML= last_number +1;
-};
-for (var i = 0 ; i < circleSelector.length; i++){
-    circleSelector[i].addEventListener ('click',function_click,false);
 }
+const circle = document.getElementById("my-circ1");
+circle.addEventListener('click', countNumber);
 
 
+// const circle2 = document.getElementById("my-circ2");
+// circle2.addEventListener('mousemove', function(event){
+//     event.target.style.borderTop ='3px solid'; 
+//     event.target.style.borderColor = '#d22b2b';
 
-function click_number(number_find) {
-    let number = 0;
-    for (let i = 0; i < number_find.length; i++);
+// })
+
+// document.getElementById("my-circ2").onmouseover = function(){
+//     document.getElementById("my-circ2").style.borderStyle = "solid";
+//     document.getElementById("my-circ2").style.borderColor = '#d22b2b';
+
+// }
+
+// document.getElementById("my-circ2").onmouseout = function(){
+//     document.getElementById("my-circ2").style.borderStyle = "none"
+// }
+// function borderChange(element, style) {
+//     element.style.borderStyle = style;
+//   }
+
+
+document.getElementById("my-circ3").onclick = function(){
+    document.getElementById("my-circ3").style.display = "none";
 }
