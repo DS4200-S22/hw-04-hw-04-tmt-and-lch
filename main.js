04d6fb3d8d4e23c7b215690c99f833ea2405717d
Liu Conghan <liuconghan@MacBook-Pro-2.local>
2022-02-17 00:28:41 -0500

    finish

--- main.js
+++ main.js
@@ -2,57 +2,33 @@ function countNumber(){
     let circleSelector = document.getElementById("counter").textContent;
     let current_num = Number(circleSelector) + 1;
     document.getElementById("counter").textContent = String(current_num);
-    // constant variable -> by id -> reference to the circle -> add lisenter and handler -> use for counter function
-
 }
+
 const circle = document.getElementById("my-circ1");
 circle.addEventListener('click', countNumber);
 
+function addBorder(){
+    document.getElementById("my-circ2").style.stroke = "black";
+    document.getElementById("my-circ2").style.strokeWidth = 13;
+}
+const circle1 = document.getElementById("my-circ2");
+circle1.addEventListener("mouseover", addBorder);
 
+function moveBorder(){
+    document.getElementById("my-circ2").style.stroke = "none";
+}
 const circle2 = document.getElementById("my-circ2");
-circle2.addEventListener('mousemove', function(event){
-    event.target.style.borderTop ='3px solid'; 
-    event.target.style.borderColor = '#d22b2b';
+circle2.addEventListener("mouseout",moveBorder);
 
-})
 
-document.getElementById("my-circ2").onmouseover = function(){
-    document.getElementById("my-circ2").style.borderStyle = "solid";
-    document.getElementById("my-circ2").style.borderColor = '#d22b2b';
-
+function circle3Click(){
+    let curOpacity = document.getElementById("my-circ3").getAttribute("class");
+    if (curOpacity == "opacity"){
+        document.getElementById("my-circ3").setAttribute("class", "opacity_1");
+    }
+    else{
+        document.getElementById("my-circ3").setAttribute("class","opacity");
+    }
 }
-
-document.getElementById("my-circ2").onmouseout = function(){
-    document.getElementById("my-circ2").style.borderStyle = "none"
-}
-function borderChange(element, style) {
-    element.style.borderStyle = style;
-  }
-
-// let circle = svg.append('my-circ2')
-//     .attr(cx,'200')
-//     .attr(cy,'200')
-//     .attr(r,'150')
-//     .attr('fill', '124, 193, 238')
-//     .on('mouseover',mouseOver)
-//     .on('mouseout',mouseOut)
-//     // .on('dblclick',dblClick)
-
-// function mouseOver() {
-//     d3.select(this).attr('stroke','black').attr('stroke-width',3);
-// }
-// function mouseOut(){
-//     d3.select(this).attr('stroke',null);
-// }
-
-
-
-
-document.getElementById("my-circ3").onclick = function(){
-    document.getElementById("my-circ3").style.display = "none";
-}
-
-// document.getElementById("my-circ3").onclick =function() {
-//     document.getElementById("my-circ3").style.display = "run-in";
-// }
-
+const circle4 = document.getElementById("my-circ3");
+circle4.addEventListener("click",circle3Click);
